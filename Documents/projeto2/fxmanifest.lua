902ad4c61a000eaa276c0ae10d9b7732d434c636
William Rolim <will_arauj_r@outlook.com>
2022-07-09 15:11:22 -0300

    first commit

---
+++ Documents/projeto2/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "F1"
+description "https://discord.gg/9aFADrAUd2"
+version "3.0"
+
+files {
+    'index.html',
+    'audio/*.mp3',
+    'css/styles.css',
+    'video/*',
+    'img/*',
+    'img/*/*',
+    'js/main.js',
+    'video/video.mp4',
+    'main.lua',
+}
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
